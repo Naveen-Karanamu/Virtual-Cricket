db9decaf8f401a8ad712f3c20be588f24ccbf818
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-03-20 20:01:00 +0530

    Fixed: Errors in Jarwis play

--- game.py
+++ game.py
@@ -10,6 +10,71 @@ print(f"Hello {name}! Welcome to the Game of Virtual Cricket 🎉🎉")
 
 ##Functios:
 
+# User's turn to bat
+def user_play(user_out, user_sum, user_turn):
+    while(user_out==0):
+        user_runs=int(input(f"Hit the {user_turn +1} ball : "))
+        comp_runs=random.randint(0,6)
+        print(f"Jarwis bowled for {comp_runs} runs")
+        if 0>user_runs or user_runs>6:
+            print("Please score runs from 0 to 6 only")
+            continue
+        
+        elif user_runs==0:
+            user_sum+=comp_runs
+            
+        elif(user_runs==comp_runs):
+            print(f"{name} you got bowled by Jarwis")
+            user_out=1
+            user_turn+=1
+        
+        else:
+            user_sum+=user_runs
+            user_turn+=1
+            
+    print(f"Total score of {name} is {user_sum} 🎉")
+    return user_sum
+
+# Computer's turn to bat
+def comp_play(comp_out, comp_sum, comp_turn, user_sum):
+    print(f"user sum is {user_sum}")
+    while(comp_out==0):
+        comp_runs=random.randint(0,6)
+        user_runs=int(input(f"Bowl for the {comp_turn + 1} ball "))
+        print(f"Jarwis scored {comp_runs}  ")
+        if(user_runs<0 or user_runs > 6):
+            print("Please ball from 0 to 6 only!")
+            continue
+        
+        elif(comp_sum>user_sum+1):
+            comp_out=1
+        
+        elif(comp_runs==user_runs):
+            print("You Bowled Jarwis 🎉 ")
+            comp_out=1
+            
+        elif(comp_runs==0):
+            comp_sum+=user_runs
+            comp_turn+=1
+            
+        else:
+            comp_sum+=comp_runs
+            comp_turn+=1
+            
+    print(f"Total score of Jarwis is : {comp_sum} ")
+    return comp_sum
+    
+# Winning condition
+def winner(comp_sum, user_sum):
+    if(comp_sum==user_sum):
+        print("The match got draw! 😑")
+        
+    elif(comp_sum>user_sum):
+        print(f"{name} you lost the game! 🤣 Better luck next time 😌")
+        
+    else:
+        print(f"{name} You won the match! cheers 🍻")
+
 # Fuction to check the bat and ball
 def play_check():
     play=input("Please Enter BAT for Batting 1st or BALL for Bowling 1st : ").upper()
@@ -22,58 +87,15 @@ def play_check():
         comp_sum=0
         user_turn=0
         comp_turn=0
-        while(user_out==0):
-            user_runs=int(input(f"Hit the {user_turn +1} ball : "))
-            comp_runs=random.randint(0,6)
-            print(f"Jarwis bowled for {comp_runs} runs")
-            if 0>user_runs or user_runs>6:
-                print("Please score runs from 0 to 6 only")
-                continue
-            
-            elif user_runs==0:
-                user_sum+=comp_runs
-                
-            elif(user_runs==comp_runs):
-                print(f"{name} you got bowled by Jarwis")
-                user_out=1
-                user_turn+=1
-            
-            else:
-                user_sum+=user_runs
-                user_turn+=1
-                
-        print(f"Total score of {name} is {user_sum} 🎉")
+        
+        user_sum=user_play(user_out, user_sum, user_turn)
+        print(user_sum)
+        
         print("Now it's Jarwis turn to Bat!")
         
-        while(comp_out==0):
-            comp_runs=random.randint(0,6)
-            user_runs=int(input(f"Bowl for the {comp_turn + 1} ball "))
-            print(f"Jarwis scored {comp_runs}  ")
-            if(user_runs<0 or user_runs > 6):
-                print("Please ball from 0 to 6 only!")
-                continue
-            elif(comp_turn==user_runs):
-                print("You Bowled Jarwis 🎉 ")
-                comp_out=1
-                
-            elif(comp_runs==0):
-                comp_sum+=user_runs
-                comp_turn+=1
-                
-            else:
-                comp_sum+=comp_runs
-                comp_turn+=1
-                
-        print(f"Total score of Jarwis is : {comp_sum} ")
+        comp_sum=comp_play(comp_out, comp_sum, comp_turn, user_sum)
         
-        if(comp_sum==user_sum):
-            print("The match got draw! 😑")
-            
-        elif(comp_sum>user_sum):
-            print(f"{name} you lost the game! 🤣 Better luck next time 😌")
-            
-        else:
-            print(f"{name} You won the match! cheers 🍻")
+        winner(comp_sum, user_sum)
 
     elif(play=='BALL'):
         print(f"Go {name} throw the Balls")
