e228720a83fd51ebc3339629475b520467706445
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-03-20 20:12:12 +0530

    Updated: Play fuction's

--- game.py
+++ game.py
@@ -10,8 +10,8 @@ print(f"Hello {name}! Welcome to the Game of Virtual Cricket 🎉🎉")
 
 ##Functios:
 
-# User's turn to bat
-def user_play(user_out, user_sum, user_turn):
+# User's turn to bat first
+def user_play_first(user_out, user_sum, user_turn):
     while(user_out==0):
         user_runs=int(input(f"Hit the {user_turn +1} ball : "))
         comp_runs=random.randint(0,6)
@@ -35,9 +35,61 @@ def user_play(user_out, user_sum, user_turn):
     print(f"Total score of {name} is {user_sum} 🎉")
     return user_sum
 
-# Computer's turn to bat
-def comp_play(comp_out, comp_sum, comp_turn, user_sum):
-    print(f"user sum is {user_sum}")
+# User's turn to bat second
+def user_play_second(user_out, user_sum, user_turn):
+    while(user_out==0):
+        user_runs=int(input(f"Hit the {user_turn +1} ball : "))
+        comp_runs=random.randint(0,6)
+        print(f"Jarwis bowled for {comp_runs} runs")
+        if 0>user_runs or user_runs>6:
+            print("Please score runs from 0 to 6 only")
+            continue
+        
+        elif user_runs==0:
+            user_sum+=comp_runs
+            
+        elif(user_runs==comp_runs):
+            print(f"{name} you got bowled by Jarwis")
+            user_out=1
+            user_turn+=1
+        
+        else:
+            user_sum+=user_runs
+            user_turn+=1
+            
+    print(f"Total score of {name} is {user_sum} 🎉")
+    return user_sum
+
+# Computer's turn to bat second
+def comp_play_second(comp_out, comp_sum, comp_turn, user_sum):
+    while(comp_out==0):
+        comp_runs=random.randint(0,6)
+        user_runs=int(input(f"Bowl for the {comp_turn + 1} ball "))
+        print(f"Jarwis scored {comp_runs}  ")
+        if(user_runs<0 or user_runs > 6):
+            print("Please ball from 0 to 6 only!")
+            continue
+        
+        elif(comp_sum>user_sum+1):
+            comp_out=1
+        
+        elif(comp_runs==user_runs):
+            print("You Bowled Jarwis 🎉 ")
+            comp_out=1
+            
+        elif(comp_runs==0):
+            comp_sum+=user_runs
+            comp_turn+=1
+            
+        else:
+            comp_sum+=comp_runs
+            comp_turn+=1
+            
+    print(f"Total score of Jarwis is : {comp_sum} ")
+    return comp_sum
+
+# Computer's turn to bat first
+def comp_play_first(comp_out, comp_sum, comp_turn, user_sum):
     while(comp_out==0):
         comp_runs=random.randint(0,6)
         user_runs=int(input(f"Bowl for the {comp_turn + 1} ball "))
@@ -88,17 +140,31 @@ def play_check():
         user_turn=0
         comp_turn=0
         
-        user_sum=user_play(user_out, user_sum, user_turn)
-        print(user_sum)
+        user_sum=user_play_first(user_out, user_sum, user_turn)
         
         print("Now it's Jarwis turn to Bat!")
         
-        comp_sum=comp_play(comp_out, comp_sum, comp_turn, user_sum)
+        comp_sum=comp_play_second(comp_out, comp_sum, comp_turn, user_sum)
         
         winner(comp_sum, user_sum)
 
     elif(play=='BALL'):
-        print(f"Go {name} throw the Balls")
+        print(f"Go {name}, throw the Balls")
+        print("Remember to Enter the runs from 0 to 6")
+        user_out=0
+        comp_out=0
+        user_sum=0
+        comp_sum=0
+        user_turn=0
+        comp_turn=0
+        
+        comp_sum=comp_play_first(comp_out, comp_sum, comp_turn, user_sum)
+        
+        print(f"Now it's your turn to Bat {name}!")
+        
+        user_sum=user_play_second(user_out, user_sum, user_turn)
+        
+        winner(comp_sum, user_sum)
         
     else:
         print("Invalid input for playing! ⚠️ Please try again!")
